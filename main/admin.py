60a76267735e07615ac86e3e672eb1d7eb2edfcb
DioDateUs <deodatussihaloho@gmail.com>
2026-09-14 14:11:28 +0700

    feat: menghapus experience di halaman depan, mengubah style di laman experience, menambah fitur agar tambah experience lewat admin, menambahkan agar bisa memasukkan foto ke mvt

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Experience
 # Register your models here.
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'organization', 'category', 'started_at', 'ended_at')
+    list_filter = ('category',)
+    search_fields = ('title', 'organization', 'description')
--- main/models.py
+++ main/models.py
@@ -12,10 +12,11 @@ class Experience(models.Model):
     ]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, help_text="Posisi / Role")
+    organization = models.CharField(max_length=255, blank=True, null=True, help_text="Nama Organisasi / Perusahaan")
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
-    thumbnail = models.URLField(blank=True, null=True)
+    photo = models.URLField(blank=True, null=True, help_text="Masukkan link URL foto/logo di sini")
     started_at = models.DateTimeField(auto_now_add=True)
     ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
